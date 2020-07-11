05c6dec1691eb608ded2bc7c6b44d3c40790c0cc
Cody Rush <rush_cody@hotmail.com>
2020-07-11 15:21:52 -0600

    created global styles

---
+++ gatsby-browser.js
@@ -0,0 +1,24 @@
+import React from "react"
+import { createGlobalStyle, ThemeProvider } from "styled-components"
+import Theme from "./src/themes/theme"
+
+const GlobalStyle = createGlobalStyle`
+  *{
+    box-sizing: border-border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  body, html {
+    font-family: ${props => props.theme.fonts.main};
+    height: 100%;
+    background: ${props => props.theme.colors.light1}
+  }
+`
+
+export const wrapRootElement = ({ element }) => (
+  <ThemeProvider theme={Theme}>
+    <GlobalStyle />
+    {element}
+  </ThemeProvider>
+)
--- src/themes/theme.js
+++ src/themes/theme.js
@@ -17,19 +17,19 @@ export default {
     tablet: "only screen and (max-width: 65rem)",
   },
   spacings: {
-      xxSmall: '.25rem',
-      xSmall: '.5rem',
-      small: '1rem',
-      medium: '2rem',
-      large: '3rem',
-      xLarge: '4rem',
-      xxLarge: '6rem',
+    xxSmall: ".25rem",
+    xSmall: ".5rem",
+    small: "1rem",
+    medium: "2rem",
+    large: "3rem",
+    xLarge: "4rem",
+    xxLarge: "6rem",
   },
   animations: {
-      button: 'box-shadown 0.3s ease',
-      link: 'color 0.2s ease'.
+    button: "box-shadown 0.3s ease",
+    link: "color 0.2s ease",
   },
   shadows: {
-      shadow1: '0px 5px 20px rgba(30,30,31, 0.05)',
-  }
+    shadow1: "0px 5px 20px rgba(30,30,31, 0.05)",
+  },
 }
