16c6dc62c09ce0328faed8be9bba81d60b5922d0
Cody Rush <rush_cody@hotmail.com>
2020-07-10 21:30:35 -0600

    added styled components and set up a theme

---
+++ src/themes/theme.js
@@ -0,0 +1,35 @@
+export default {
+  fonts: {
+    main: "Muli, sans-serif",
+    code: "Roboto Mono, monospace",
+  },
+  colors: {
+    main1: "#4d9ee0",
+    main2: "#e5f1fa",
+    dark1: "#1e1e1f",
+    dark2: "#414244",
+    dark3: "#a1a2a5",
+    light1: "#f7f7f7",
+    light2: "#fcfcfc",
+  },
+  breakpoints: {
+    mobile: "only screen and (max-width: 50rem)",
+    tablet: "only screen and (max-width: 65rem)",
+  },
+  spacings: {
+      xxSmall: '.25rem',
+      xSmall: '.5rem',
+      small: '1rem',
+      medium: '2rem',
+      large: '3rem',
+      xLarge: '4rem',
+      xxLarge: '6rem',
+  },
+  animations: {
+      button: 'box-shadown 0.3s ease',
+      link: 'color 0.2s ease'.
+  },
+  shadows: {
+      shadow1: '0px 5px 20px rgba(30,30,31, 0.05)',
+  }
+}
